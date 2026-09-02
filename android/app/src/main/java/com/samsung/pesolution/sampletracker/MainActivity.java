5a86090ab964ffb1cbd672f513441e7fe70f8fe5
endri Susanto <endrisusantosekarang@gmail.com>
2026-09-02 07:06:04 +0700

    feat(android): enable true immersive fullscreen mode hiding navbar and status bar

--- android/app/src/main/java/com/samsung/pesolution/sampletracker/MainActivity.java
+++ android/app/src/main/java/com/samsung/pesolution/sampletracker/MainActivity.java
@@ -1,5 +1,35 @@
 package com.samsung.pesolution.sampletracker;
 
+import android.os.Bundle;
+import android.view.View;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideSystemUI();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        if (controller != null) {
+            // Sembunyikan status bar (atas) dan navigation bar (bawah)
+            controller.hide(WindowInsetsCompat.Type.systemBars());
+            // Mode immersive: status bar & navbar hanya muncul sementara saat di-swipe dari pinggir layar lalu sembunyi lagi otomatis
+            controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
+    }
+}
